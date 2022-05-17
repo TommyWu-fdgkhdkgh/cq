1b45bd28844e4399d90aec99dab2ca0c125851c2
chien-hsing.wu <fdgkhdkgh@gmail.com>
2022-05-17 08:33:41 +0800

    add more unit test

--- test/test1.cpp
+++ test/test1.cpp
@@ -2,6 +2,12 @@
 #include "gmock/gmock.h"
 #include "cq.h"
 
+TEST(testCq, defaultInit) {
+    cq<int> newcq;
+    EXPECT_EQ(newcq.getSize(), 0);
+    EXPECT_EQ(newcq.getNumOfElements(), 0);
+}
+
 TEST(testCq, returnSize) {
     // cq<int> newcq(10);
     cq<int> newcq = cq<int>(10);
@@ -31,8 +37,94 @@ TEST(testCq, getNumOfElements) {
     EXPECT_EQ(newcq.getNumOfElements(), 0);
 }
 
+TEST(testCq, enQueueAndPopQueue) {
+    cq<int> newcq = cq<int>(5);
+    newcq.enQueueFromHead(10);
+    EXPECT_EQ(newcq.getNumOfElements(), 1);
+    newcq.enQueueFromHead(20);
+    EXPECT_EQ(newcq.getNumOfElements(), 2);
+    newcq.enQueue(30);
+    EXPECT_EQ(newcq.getNumOfElements(), 3);
+    newcq.enQueue(40);
+    EXPECT_EQ(newcq.getNumOfElements(), 4);
+    newcq.enQueueFromHead(50);
+    EXPECT_EQ(newcq.getNumOfElements(), 5);
+    
+    int res;
+    res = newcq.popQueue();
+    EXPECT_EQ(newcq.getNumOfElements(), 4);
+    EXPECT_EQ(res, 50);
+
+    res = newcq.popQueue();
+    EXPECT_EQ(newcq.getNumOfElements(), 3);
+    EXPECT_EQ(res, 20);
+
+    res = newcq.popQueue();
+    EXPECT_EQ(newcq.getNumOfElements(), 2);
+    EXPECT_EQ(res, 10);
+
+    res = newcq.popQueue();
+    EXPECT_EQ(newcq.getNumOfElements(), 1);
+    EXPECT_EQ(res, 30);
+
+    res = newcq.popQueue();
+    EXPECT_EQ(newcq.getNumOfElements(), 0);
+    EXPECT_EQ(res, 40);
+}
+
+TEST(testCq, enQueueAndPopQueueAndPeek) {
+    cq<int> newcq = cq<int>(5);
+    int res;
+    newcq.enQueueFromHead(10);
+    EXPECT_EQ(newcq.peekQueue(), 10); 
+    EXPECT_EQ(newcq.getNumOfElements(), 1);
+    newcq.enQueueFromHead(20);
+    EXPECT_EQ(newcq.peekQueue(), 20); 
+    EXPECT_EQ(newcq.getNumOfElements(), 2);
+    newcq.enQueue(30);
+    EXPECT_EQ(newcq.peekQueue(), 20); 
+    EXPECT_EQ(newcq.getNumOfElements(), 3);
+    newcq.enQueue(40);
+    EXPECT_EQ(newcq.peekQueue(), 20); 
+    EXPECT_EQ(newcq.getNumOfElements(), 4);
+    newcq.enQueueFromHead(50);
+    EXPECT_EQ(newcq.peekQueue(), 50); 
+    EXPECT_EQ(newcq.getNumOfElements(), 5);
+    
+    res = newcq.popQueue();
+    EXPECT_EQ(newcq.getNumOfElements(), 4);
+    EXPECT_EQ(res, 50);
+    EXPECT_EQ(newcq.peekQueue(), 20); 
+
+    res = newcq.popQueue();
+    EXPECT_EQ(newcq.getNumOfElements(), 3);
+    EXPECT_EQ(res, 20);
+    EXPECT_EQ(newcq.peekQueue(), 10); 
+
+    res = newcq.popQueue();
+    EXPECT_EQ(newcq.getNumOfElements(), 2);
+    EXPECT_EQ(res, 10);
+    EXPECT_EQ(newcq.peekQueue(), 30);
+ 
+    res = newcq.popQueue();
+    EXPECT_EQ(newcq.getNumOfElements(), 1);
+    EXPECT_EQ(res, 30);
+    EXPECT_EQ(newcq.peekQueue(), 40);
+
+    res = newcq.popQueue();
+    EXPECT_EQ(newcq.getNumOfElements(), 0);
+    EXPECT_EQ(res, 40);
+}
 
 int main(int argc, char *argv[]) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
+
+
+
+
+
+
+
+
